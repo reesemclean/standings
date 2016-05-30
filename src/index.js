416eb6e7c1547f815085f8850a592d92097681c0
Reese McLean <reese.mclean@me.com>
2016-05-30 10:42:56 -0700

    Added React-Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router'
 import App from './components/Main';
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App} />
+  </Router>
+), document.getElementById('app'))
